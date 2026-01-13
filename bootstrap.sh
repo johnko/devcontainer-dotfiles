e15fd729b84bc4fff3728fc65da4a5c577212051
johnko <279736+johnko@users.noreply.github.com>
2026-01-13 02:10:10 -0500

    use universal devcontainer image

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,8 +14,4 @@ done
 
 set -x
 
-OPENCODE_VERSION=1.1.12      # renovate: datasource=github-releases depName=anomalyco/opencode packageName=anomalyco/opencode
-DEVCONTAINERS_VERSION=0.80.3 # renovate: datasource=github-releases depName=devcontainers/cli packageName=devcontainers/cli
-
-npm install --global opencode-ai@$OPENCODE_VERSION
-npm install --global @devcontainers/cli@$DEVCONTAINERS_VERSION
+go install github.com/mikefarah/yq/v4@latest
