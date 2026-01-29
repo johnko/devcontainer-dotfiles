21d1dd60488d2bc1209f37f9ca834e40ad8c33e7
johnko <279736+johnko@users.noreply.github.com>
2026-01-29 02:45:37 -0500

    backport

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,3 +6,12 @@ sudo apt install --yes shellcheck
 
 go install github.com/mikefarah/yq/v4@v4.50.1
 go install mvdan.cc/sh/v3/cmd/shfmt@v3.12.0
+
+# renovate: datasource=github-releases depName=anomalyco/opencode packageName=anomalyco/opencode
+OPENCODE_VERSION=1.1.12
+
+# renovate: datasource=github-releases depName=devcontainers/cli packageName=devcontainers/cli
+DEVCONTAINERS_VERSION=0.80.3
+
+npm install --global opencode-ai@$OPENCODE_VERSION
+npm install --global @devcontainers/cli@$DEVCONTAINERS_VERSION
